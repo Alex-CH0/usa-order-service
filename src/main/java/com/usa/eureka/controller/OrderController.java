f815da6a65c273bfc979f6f5711293d05f985379
Alex-CHO <hyungjin98@gmail.com>
2024-12-09 16:59:41 +0900

    added endpoint to check port number and lb

--- src/main/java/com/usa/eureka/controller/OrderController.java
+++ src/main/java/com/usa/eureka/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.usa.eureka.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +16,10 @@ public class OrderController {
     @GetMapping("/one")
     public String getOne() {
         return "rand!";
+    }
+
+    @GetMapping("/info")
+    public String getInfo(HttpServletRequest request) {
+        return "Server on Port :" + request.getServerPort();
     }
 }
