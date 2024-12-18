6e3ec70beb6afb5006029d883622199094a43fb0
Alex-CHO <hyungjin98@gmail.com>
2024-12-18 14:15:29 +0900

    finding kafka bug

--- src/main/java/com/usa/eureka/kafka/KafkaProducerConfig.java
+++ src/main/java/com/usa/eureka/kafka/KafkaProducerConfig.java
@@ -3,6 +3,8 @@ package com.usa.eureka.kafka;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -10,6 +12,8 @@ import org.springframework.kafka.core.ProducerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
+@EnableKafka
+@Configuration
 public class KafkaProducerConfig {
     @Bean
     public ProducerFactory<String, String> producerFactory() {
@@ -22,7 +26,7 @@ public class KafkaProducerConfig {
     }
 
     @Bean
-    public KafkaTemplate<String, String> kafkaTemplate(){
+    public KafkaTemplate<String, String> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 
